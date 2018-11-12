613d8cb1185b979e321974bdd25b37135b85ac58
aaronwharton <aircitybruno@gmail.com>
2018-11-12 12:35:49 +0800

    Update doc.go

---
+++ doc.go
@@ -0,0 +1,9 @@
+// Github: https://github.com/AaronWharton/mdreminder
+
+// mdreminder is a project that detects whether urls in projects'
+// instructions(e.g. *.md files) are valid.
+//
+// It is intended to automatically find the invalid links in files and
+// inform user to correct the url. When you migrate your project, the
+// urls seem not to work, mdreminder can amend the url in time.
+package mdreminder
